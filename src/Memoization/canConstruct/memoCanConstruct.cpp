53e89cac5cd38d4ae90a53bbc7506e1aa448e4a4
CSjianbel <jiankarlo99@gmail.com>
2021-02-17 08:55:41 +0800

    canConstruct problem completed! :)

---
+++ src/Memoization/canConstruct/memoCanConstruct.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <map>
+using namespace std;
+
+class CanConstruct
+{
+public:
+    bool canConstruct(string target, vector<string>& prefixes)
+    {
+        if (memo.find(target) != memo.end())
+            return memo[target];
+        if (target == "")
+            return true;
+        
+        for (string& prefix : prefixes)
+        {
+            if (prefix.length() > target.length())
+                continue;
+            
+            if (prefix == target.substr(0, prefix.length()))
+                if (canConstruct(target.substr(prefix.length(), target.length()), prefixes))
+                    return memo[target] = true;
+        }
+
+        return memo[target] = false;
+    }
+private:
+    unordered_map<string,bool> memo;
+};
+
+void printVector(vector<string>& v);
+vector<string> copyVector(const vector<string> v);
+void printBool(bool b);
+
+int main(void)
+{
+    map<pair<string,vector<string>>,bool> tests({
+        {{"abcdef", {"ab", "abc", "cd", "def", "abcd"}}, true},
+        {{"skateboard", {"bo", "rd", "ate", "t", "ska", "sk", "boar"}}, false},
+        {{"enterapotentpot", {"a", "p", "ent", "enter", "ot", "o", "t"}}, true},
+        {
+            {
+                "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", 
+                {"e", "ee", "eee", "eeee", "eeeee", "eeeeee", "eeeeeee"}
+            }, false
+        }
+    });
+
+    for (map<pair<string,vector<string>>,bool>::iterator it = tests.begin(); it != tests.end(); it++)
+    {
+        CanConstruct c;
+
+        vector<string> tmp = copyVector(it->first.second);
+        cout << "Testcase: " << it->first.first << " : ";
+        printVector(tmp);
+        cout << " - Expected: ";
+        printBool(it->second);
+        cout << "\nResult: ";
+        bool res = c.canConstruct(it->first.first, tmp);
+        printBool(res);
+        cout << (res == it->second ? "\t:)\n\n" : "\t:(\n\n");
+    }
+
+    return 0;
+}
+
+void printVector(vector<string>& v)
+{
+	cout << "[";
+	for (vector<string>::iterator itr = v.begin(); itr != v.end(); itr++)
+	{
+		cout << *itr;
+		if (itr + 1 != v.end())
+			cout << ", ";
+	}
+	cout << "]";
+}
+
+vector<string> copyVector(const vector<string> v)
+{
+	vector<string> newVector;
+	for (int i = 0; i < v.size(); i++)
+		newVector.push_back(v[i]);
+	return newVector;
+}
+
+void printBool(bool b)
+{
+    cout << (b ? "true" : "false");
+}
